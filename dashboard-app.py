3568b706cbb1f9d81a72e509d611ee32719201aa
Brian Sanchez <127140204+bsanche-3@users.noreply.github.com>
2025-02-17 07:10:53 -0500

    Update dashboard-app.py
    Cargar el titulo en streamlit

--- dashboard-app.py
+++ dashboard-app.py
@@ -1,1 +1,16 @@
+import streamlit as st
+import pandas as pd
+import munpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+from datetime import datetime
 
+#1 Configuaracion incial de la aplicacion
+st.set_page_config(
+  page_title = "Dashboard Interactivo",
+  page_ icon=":)",
+  layaut="wide"
+)
+st.title("Dashboard Interactivo con Streamlit")
+st.sidebar.title("Opciones de Navegacion")
