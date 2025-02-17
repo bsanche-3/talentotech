68cfd44b90c9ceadb0fff65f766084ea96ccc620
Brian Sanchez <127140204+bsanche-3@users.noreply.github.com>
2025-02-17 07:16:55 -0500

    Update dashboard-app.py
    cambios para ver el icono

--- dashboard-app.py
+++ dashboard-app.py
@@ -1,16 +1,18 @@
-import streamlit as st
+import streamlit as st 
 import pandas as pd
-import munpy as np
+import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
+import seaborn as sns 
 import plotly.express as px
 from datetime import datetime
 
-#1 Configuaracion incial de la aplicacion
-st.set_page_config(
-  page_title = "Dashboard Interactivo",
-  page_ icon=":)",
-  layaut="wide"
+# 1. Configuración inicial de la aplicación
+
+st. set_page_config(
+  page_title="Dashboard Interactivo",
+  page_icon="🔥",
+  layout="wide"
 )
-st.title("Dashboard Interactivo con Streamlit")
-st.sidebar.title("Opciones de Navegacion")
+
+st.title("🔥 Dashboard Interactivo con Streamlit")
+st.sidebar.title("🚀 Opciones de Navegación")
